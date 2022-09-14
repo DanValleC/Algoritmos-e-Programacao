fd0db762f7091208f2fe92abcec1ebfb39a8c28b
DanValleC <ddovale4@gmail.com>
2022-09-14 20:37:17 -0300

    lista_questao1_atualizada

--- horas.cpp.c
+++ horas.cpp.c
@@ -1,12 +1,14 @@
-#include <stdio.h> //biblioteca para os comandos (funções) printf e scanf
+#include <stdio.h> //biblioteca para os comandos (funï¿½ï¿½es) printf e scanf
 
 int main(){ //inicio calcula Hora
 
-    int horas, minutos; //inteiro horas, minutos;
+    int horas, minutos, segundos; //inteiro horas, minutos;
     printf("Digite a quantidade de horas: "); //escreva("Digite a quantidade de horas");
     scanf("%i", &horas); //leia(horas);
     minutos = horas * 60; //minutos = horas * 60;
-    printf("minutos: %i", minutos); //escreva(minutos);
+    segundos = minutos * 60; //segundos = minutos * 60;
+    printf("\nMinutos: %i", minutos); //escreva(minutos);
+    printf("\nSegundos: %i", segundos); //escreva(segundos);
     
     return 0;
 }//fim
